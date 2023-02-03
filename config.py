5a8c31dfcacd9de0c6a06f2b8195fa648fa5f688
matt.macarty@gmail.com <matt.macarty@gmail.com>
2023-02-03 18:24:03 -0500

    Demo Files

--- config.py
+++ config.py
@@ -1,2 +1,11 @@
-API_KEY = "PKQ9Z7LDMUQ9U63NYFZR"
-SECRET_KEY = "TRC3qSm2oJKMfe2y7PyLyBQDrZSkEDeBfcDeqVkA"
+API_KEY = ""
+SECRET_KEY = ""
+
+ALPACA_CONFIG = {
+# Put your own Alpaca key here:
+"API_KEY": "",
+# Put your own Alpaca secret here:
+"API_SECRET": "",
+# If you want to go live, you must change this
+"ENDPOINT": "https://paper-api.alpaca.markets",
+}
---
+++ lumibot_buy_hold.py
@@ -0,0 +1,47 @@
+from config import ALPACA_CONFIG
+from datetime import datetime
+from lumibot.backtesting import YahooDataBacktesting
+from lumibot.brokers import Alpaca
+from lumibot.strategies import Strategy
+from lumibot.traders import Trader
+
+
+class BuyHold(Strategy):
+
+    def initialize(self):
+        self.sleeptime = "1D"
+
+    def on_trading_iteration(self):
+        if self.first_iteration:
+            symbol = "GOOG"
+            price = self.get_last_price(symbol)
+            quantity = self.cash  // price
+            order = self.create_order(symbol, quantity, "buy")
+            self.submit_order(order)
+
+if __name__ == "__main__":
+    trade = False
+    if trade:
+        broker = Alpaca(ALPACA_CONFIG)
+        strategy = BuyHold(broker=broker)
+        trader = Trader()
+        trader.add_strategy(strategy)
+        trader.run_all()
+    else:
+        start = datetime(2022,1,1)
+        end = datetime(2022,12,31)
+        BuyHold.backtest(
+            YahooDataBacktesting,
+            start,
+            end
+        )    
+
+
+
+
+
+
+
+
+
+
